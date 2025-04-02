bac170d254e3c9e5a8d5b61391518d6dc4ce21ad
Kristiyan <kristiyan_hristov04@abv.bg>
2025-04-02 23:49:15 +0300

    SongPlayer is now optimized for checking if song is liked or not

--- src/components/preview-song/song-player/SongPlayer.jsx
+++ src/components/preview-song/song-player/SongPlayer.jsx
@@ -3,9 +3,9 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { useState, useEffect, useContext } from 'react';
 import AuthContext from '../../../context/AuthContext';
-import { supabase } from '../../../supabase';
 import CustomAudioPlayer from './CustomAudioPlayer';
 import { Link } from 'react-router-dom';
+import FavouriteSongsContext from '../../../context/FavouriteSongsContext';
 
 export default function SongPlayer({
     songId,
@@ -19,37 +19,18 @@ export default function SongPlayer({
     const [user] = useContext(AuthContext);
     const [isLiked, setIsLiked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const { favouriteSongs, toggleFavouriteSong } = useContext(FavouriteSongsContext);
 
     useEffect(() => {
         try {
             if (user.id) {
-                const checkIfSongLiked = async () => {
-                    const { data, error } = await supabase
-                        .from('users_favourite_songs')
-                        .select()
-                        .eq('user_id', user.id)
-                        .eq('song_id', songId);
-
-                    if (error) {
-                        throw new Error(error.message);
-                    }
-
-                    if (data[0]) {
-                        setIsLiked(true);
-                    }
-
-                    setIsLoading(false);
-                }
-
-                checkIfSongLiked();
-            } else {
+                const isLiked = favouriteSongs.includes(songId);
+                setIsLiked(isLiked);
                 setIsLoading(false);
             }
         } catch (e) {
             console.error(e.message);
         }
-
-
     }, []);
 
     const clickHandlerAddToFavourite = async () => {
@@ -57,34 +38,8 @@ export default function SongPlayer({
             navigate('/login');
         }
 
-        try {
-            if (isLiked) {
-                const response = await supabase
-                    .from('users_favourite_songs')
-                    .delete()
-                    .eq('user_id', user.id)
-                    .eq('song_id', songId);
-
-                console.log(response);
-                setIsLiked(false);
-            } else {
-                const { data, error } = await supabase
-                    .from('users_favourite_songs')
-                    .insert({
-                        user_id: user.id,
-                        song_id: songId,
-                    });
-
-                if (error) {
-                    throw new Error(error.message);
-                }
-
-                setIsLiked(true);
-            }
-        } catch (e) {
-            console.error(e.message);
-        }
-
+        await toggleFavouriteSong(songId);
+        setIsLiked(!isLiked);
     }
 
     return (
@@ -109,8 +64,8 @@ export default function SongPlayer({
                                 <div className={styles["artist-names"]}>
                                     {songArtists.map((artist, index) => (
                                         <span key={artist.id}>
-                                            <Link 
-                                                to={`/artist/${artist.id}`} 
+                                            <Link
+                                                to={`/artist/${artist.id}`}
                                                 className={styles["artist-link"]}
                                             >
                                                 {artist.name}
